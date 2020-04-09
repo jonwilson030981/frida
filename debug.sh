760d54cbf6e46235fd5ad7d90bcd382241d6479a
Your Name <you@example.com>
2020-04-09 14:40:59 +0100

    Added script to run reloc test

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@ make -f Makefile.linux.mk gum-linux-arm && \
 QEMU_LD_PREFIX=/usr/arm-linux-gnueabi qemu-arm \
     -g 1234 \
     ./build/tmp_thin-linux-arm/frida-gum/tests/gum-tests \
-    -p /Core/Stalker/performance \
+    -p /Core/Stalker/can_follow_workload \
     &
 
 sleep 0.5
